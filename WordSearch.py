13d09b658aa47fe06e27fdfccdb040772c341acc
Tom Lancaster <lancaster.c.tom@gmail.com>
2020-12-15 15:09:08 +0000

    Added further comment about multi threading (testing has and will follow to double check claim)

--- WordSearch.py
+++ WordSearch.py
@@ -19,5 +19,11 @@ for word in words_to_find:
 Provided there wasn't a requirement to loop through the words in a specific order, then you could simply break the list into n sub-lists, 
 where n is the number of threads capable of being used in your system. This has been demonstrated and tested in the test_WordSearch.py file
 
+There are two other potential places for multithreading/multiprocessing could be beneficial from within the code:
+1) Determination of horizontals/verticals (~4/5) (see testing/multi-processed-init branch)
+2) Checking each any() from is_present
+
+However, given these operations individually take a very small amount of time, there's a high probability that it would in fact slow the processing down
+
 Note: It's important to consider data access when using threading, however in the above example, data is only ever being read
 """
