cd147fd5503236f82302742ee9353864a41e80e9
Tom Lancaster <lancaster.c.tom@gmail.com>
2020-12-15 15:36:44 +0000

    Moved comment to new README

--- WordSearch.py
+++ WordSearch.py
@@ -6,24 +6,3 @@ class WordSearch(object):
 
     def is_present(self, word):
         return any(word in line for line in self.__horizontals) or any(word in line for line in self.__verticals)
-
-
-"""
-Bonus Question:
-The primary place where parallelisation could be used to speed up execution with a multi core system is with the class usage code:
-
-for word in words_to_find:
-    if ws.is_present(word):
-        print "found {}".format(word)
-
-Provided there wasn't a requirement to loop through the words in a specific order, then you could simply break the list into n sub-lists, 
-where n is the number of threads capable of being used in your system. This has been demonstrated and tested in the test_WordSearch.py file
-
-There are two other potential places for multithreading/multiprocessing could be beneficial from within the code:
-1) Determination of horizontals/verticals (~4/5) (see testing/multi-processed-init branch)
-2) Checking each any() from is_present (see testing/multi-processed-is-present branch)
-
-However, given these operations individually take a very small amount of time, there's a high probability that it would in fact slow the processing down
-
-Note: It's important to consider data access when using threading, however in the above example, data is only ever being read
-"""
