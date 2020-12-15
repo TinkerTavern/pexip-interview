e692dc5f94e45ab05bde7253ba0550ed7b523745
Tom Lancaster <lancaster.c.tom@gmail.com>
2020-12-15 14:49:21 +0000

    Added extra tests and supporting methods designed for threading testing TODO: LOTS of duplicate code! TODO: Currently the tests fail, turn into timing tests

--- test_WordSearch.py
+++ test_WordSearch.py
@@ -1,3 +1,7 @@
+import random
+import string
+import threading
+from datetime import datetime
 from unittest import TestCase
 
 from WordSearch import WordSearch
@@ -6,7 +10,36 @@ from WordSearch import WordSearch
 def do_search(grid, words_to_find):
     ws = WordSearch(grid)
     output = []
+    start_present = datetime.now()
     for word in words_to_find:
+        if ws.is_present(word):
+            output.append("found {}".format(word))
+    print("Standard = ", datetime.now() - start_present)
+    return output
+
+
+def do_search_multi(grid, words_to_find):
+    n = 16  # no of threads
+    size = int(len(words_to_find) / n)
+    ws = WordSearch(grid)
+
+    threads = []
+    start = datetime.now()
+    for i in range(n):
+        arr = words_to_find[i * size:(i + 1) * size]
+
+    thr = threading.Thread(target=find_words, args=(ws, arr,))
+    thr.start()
+    threads.append(thr)
+
+    for thread in threads:
+        thread.join()
+    print("Threaded = ", datetime.now() - start)
+
+
+def find_words(ws, list):
+    output = []
+    for word in list:
         if ws.is_present(word):
             output.append("found {}".format(word))
     return output
@@ -29,3 +62,32 @@ class TestWordSearch(TestCase):
                "jsdkjw3rd1"
         words_to_find = ["word1", "word2", "word3", "thing"]
         self.assertEquals(do_search(grid, words_to_find), correct_output(["word1", "word2", "word3"]))
+
+    def test_small(self):
+        letters = string.ascii_lowercase
+        grid = ''.join(random.choice(letters) for _ in range(10000))
+        words_to_find = [(''.join(random.choice(letters) for _ in range(random.randint(1, 12)))) for _ in
+                         range(10000)]
+        start = datetime.now()
+        self.assertEquals(do_search(grid, words_to_find), correct_output([""]))
+        print(datetime.now() - start)
+
+    def test_med(self):
+        letters = string.ascii_lowercase
+        grid = ''.join(random.choice(letters) for _ in range(1000000))
+        words_to_find = [(''.join(random.choice(letters) for _ in range(random.randint(1, 15)))) for _ in
+                         range(100000)]
+        start = datetime.now()
+        do_search_multi(grid, words_to_find), correct_output([""])
+        self.assertEquals(do_search(grid, words_to_find), correct_output([""]))
+        print(datetime.now() - start)
+
+    def test_full(self):
+        letters = string.ascii_lowercase
+        grid = ''.join(random.choice(letters) for _ in range(100000000))  # 10^4 in each direction
+        words_to_find = [(''.join(random.choice(letters) for _ in range(random.randint(1, 24)))) for _ in
+                         range(1000000)]
+        print("Start testing")
+        start = datetime.now()
+        self.assertEquals(do_search(grid, words_to_find), correct_output([""]))
+        print(datetime.now() - start)
