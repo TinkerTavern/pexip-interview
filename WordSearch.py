87bfe569cec5f98ada01219529e356ff6da713f4
Tom Lancaster <lancaster.c.tom@gmail.com>
2020-12-15 15:20:31 +0000

    Quick testing to see if multi threading would help is_present. Once again, the time taken to start and join two processes far outweigh the potential speed improvement from splitting the task up (in medium testing on my PC, a few hundred times slower)

--- WordSearch.py
+++ WordSearch.py
@@ -1,11 +1,33 @@
+import multiprocessing
+from datetime import datetime
+
+
 class WordSearch(object):
     def __init__(self, grid):
         self.__ROW_LENGTH = int(len(grid) ** 0.5)  # Intended to be fixed, but dynamically determined for testing
         self.__horizontals = [grid[i:i + self.__ROW_LENGTH] for i in range(0, len(grid), self.__ROW_LENGTH)]
-        self.__verticals = [''.join([grid[j + i] for j in range(0, len(grid), self.__ROW_LENGTH)]) for i in range(self.__ROW_LENGTH)]
+        self.__verticals = [''.join([grid[j + i] for j in range(0, len(grid), self.__ROW_LENGTH)]) for i in
+                            range(self.__ROW_LENGTH)]
 
     def is_present(self, word):
-        return any(word in line for line in self.__horizontals) or any(word in line for line in self.__verticals)
+        start = datetime.now()
+        process1 = multiprocessing.Process(target=self.check_horizontals, args=(word,))
+        process2 = multiprocessing.Process(target=self.check_verticals, args=(word,))
+        process1.start()
+        process2.start()
+        process1.join()
+        process2.join()
+        print("Parallel = ", datetime.now() - start)
+        start = datetime.now()
+        ans = any(word in line for line in self.__horizontals) or any(word in line for line in self.__verticals)
+        print("Linear = ", datetime.now() - start)
+        return ans
+
+    def check_horizontals(self, word):
+        return any(word in line for line in self.__horizontals)
+
+    def check_verticals(self, word):
+        return any(word in line for line in self.__verticals)
 
 
 """
@@ -21,7 +43,7 @@ where n is the number of threads capable of being used in your system. This has 
 
 There are two other potential places for multithreading/multiprocessing could be beneficial from within the code:
 1) Determination of horizontals/verticals (~4/5) (see testing/multi-processed-init branch)
-2) Checking each any() from is_present
+2) Checking each any() from is_present (see testing/multi-processed-is-present branch)
 
 However, given these operations individually take a very small amount of time, there's a high probability that it would in fact slow the processing down
 
